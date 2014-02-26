73c19812ed961c083ad5289cb7f1ecce888f38d6
Nate Finch <nate.finch@gmail.com>
2014-02-26 17:08:05 -0500

    start using labix.org/pipe

--- sh.go
+++ sh.go
@@ -1,86 +1,103 @@
 // Package sh is intended to make working with shell commands more shell-like.
-// This package is basically just syntactic sugar wrapped around os/exec, but it
-// can make your code a lot easier to read when you have a simple shell-like
-// line rather than a huge mess of pipes and commands.
+// This package is basically just syntactic sugar wrapped around
+// labix.org/v2/pipe, which in turn just wraps os/exec, but it can make your
+// code a lot easier to read when you have a simple shell-like line rather than
+// a huge mess of pipes and commands and conversions etc.
 //
+//	// create functions that runs the echo, grep, and wc shell commands
 //	echo := sh.Cmd("echo")
+//	grep := sh.Cmd("grep")
+//	wc := sh.Cmd("wc")
 //
-//	fmt.Print(echo("Hi there!"))
-//	// output:
-//	// Hi there!
+//	// run echo, pipe the output through grep and then through wc
+//	// effectively the same as
+//	// $ echo Hi there! | grep -o Hi | wc -w
+//	fmt.Print(sh.Pipe(echo("Hi there!"), grep("-o", "Hi"), wc("-w")))
 //
-//	grep := sh.Cmd("grep", "-o")
-//	wc := sh.Cmd("wc", "-w")
-//
-//	fmt.Print(sh.Pipe(echo("Hi there!"), grep("Hi"), wc()))
 //	// output:
 //	// 1
 package sh
 
 import (
-	"bufio"
-	"os/exec"
+	"io"
+	"strings"
+
+	"labix.org/v2/pipe"
 )
 
-// Cmd returns a function that runs the given command with the given args.  The
-// args that are passed to Cmd cannot be overridden, but the function that is
-// returned can add more arguments when you call it.
-func Cmd(name string, args ...string) func(args ...string) Command {
-	c := exec.Command(name, args...)
-
-	f := func(args ...string) Command {
-		c.Args = append(c.Args, args...)
-
-		return func(args ...string) (string, error) {
-			if len(args) > 0 {
-				p, _ := c.StdinPipe()
-				b := bufio.NewWriter(p)
-				go func() { b.WriteString(args[0]); b.Flush(); p.Close() }()
-			}
-			out, err := c.CombinedOutput()
-			return string(out), err
-		}
+// Cmd returns a function that will return an Executable for the given command
+// with the given args.  This is a convenience for defining functions for
+// commonly reused Executables, such as grep, ls, mkdir, etc.
+//
+// The args that are passed to Cmd are passed to the Executable when the
+// returned function is run, allowing you to pre-set some common arguments.
+func Cmd(name string, args0 ...string) func(args ...string) Executable {
+	return func(args1 ...string) Executable {
+		return Executable{pipe.Exec(name, append(args0, args1...)...)}
 	}
-
-	return f
 }
 
-// Pipe connects the output of one Command to the input of the next Command,
-// returning immediately if any of the commands produce an error.  The result is
-// a function that returns the output of the last function run, and any error it
-// might have had.  Alternatively, you can call String() on the result to get
-// just the output. You may also pass the result into a fmt.Print-style function
-// as if it were a string.
-func Pipe(cmds ...Command) pipeResult {
-	stdin := ""
-	var err error
-	for _, cmd := range cmds {
-		stdin, err = cmd(stdin)
-		if err != nil {
-			break
-		}
+// Dump returns an excutable that will read the given file and dump its contents
+// as the Executable's stdout.
+func Dump(filename string) Executable {
+	return Executable{pipe.ReadFile(filename)}
+}
+
+// Read returns an executable that will read from the given reader and use it as
+// the Executable's stdout.
+func Read(r io.Reader) Executable {
+	return Executable{pipe.Read(r)}
+}
+
+// Pipe connects the output of one Executable to the input of the next
+// Executable in the list.  The result is an Executable that, when run, returns
+// the output of the last Executable run, and any error it might have had.
+//
+// If any of the Executables fails, no further Executables are run, and the
+// failing Executable's stderr and error are returned.
+func Pipe(cmds ...Executable) Executable {
+	ps := make([]pipe.Pipe, len(cmds))
+	for i, c := range cmds {
+		ps[i] = c.Pipe
 	}
-	return func() (string, error) { return stdin, err }
+	return Executable{pipe.Line(ps...)}
 }
 
-// Command is the type that is returned from running a Cmd.  You can pass it
-// into a fmt.Print style function as if it were a string and it'll do the right
-// thing, or you can execute it to get the output and any errors, or call
-// String() to just get the output.
-type Command func(arg ...string) (string, error)
-
-// String implements Stringer.String
-func (c Command) String() string {
-	s, _ := c()
-	return s
+// PipeWith functions like Pipe, but runs the first command with stdin as the
+// input.
+func PipeWith(stdin string, cmds ...Executable) Executable {
+	ps := make([]pipe.Pipe, len(cmds)+1)
+	ps[0] = pipe.Read(strings.NewReader(stdin))
+	for i, c := range cmds {
+		ps[i+1] = c.Pipe
+	}
+	return Executable{pipe.Line(ps...)}
 }
 
-// pipeResult is a function that you can call to get a string and an error, or
-// call String() to get just the string.
-type pipeResult func() (string, error)
+// Executable is a runnable construct.  You can run it by calling Run(), or by
+// calling String() (which is automatically done when passing it into a
+// fmt.Print style function).  It can be passed into Pipe to form a chain of
+// Executables that are executed in series.
+type Executable struct {
+	pipe.Pipe
+}
 
-// String implements Stringer.String
-func (p pipeResult) String() string {
-	s, _ := p()
-	return s
+// Run executes the command with the given string as standard input, and returns
+// stdout and a nil error on success, or stderr and a non-nil error on failure.
+func (c Executable) Run(stdin string) (string, error) {
+	stdout, stderr, err := pipe.DividedOutput(
+		pipe.Line(pipe.Read(strings.NewReader(stdin)), c.Pipe),
+	)
+	if err != nil {
+		return string(stderr), err
+	}
+	return string(stdout), nil
+}
+
+// String runs the Executable and returns the standard output as a string,
+// ignoring any error.  This is most useful for passing an executable into a
+// fmt.Print style function.
+func (c Executable) String() string {
+	s, _ := pipe.Output(c.Pipe)
+	return string(s)
 }
--- sh_test.go
+++ sh_test.go
@@ -2,9 +2,31 @@ package sh_test
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/natefinch/sh"
 )
+
+const SWCrawl = `
+A long time ago, in a galaxy far, far away....
+
+It is a period of civil war. Rebel
+spaceships, striking from a hidden
+base, have won their first victory
+against the evil Galactic Empire.
+
+During the battle, Rebel spies managed
+to steal secret plans to the Empire's
+ultimate weapon, the Death Star, an
+armored space station with enough
+power to destroy an entire planet.
+
+Pursued by the Empire's sinister agents,
+Princess Leia races home aboard her
+starship, custodian of the stolen plans
+that can save her people and restore
+freedom to the galaxy...`
 
 func ExampleCmd() {
 	echo := sh.Cmd("echo")
@@ -16,24 +38,25 @@ func ExampleCmd() {
 
 func ExamplePipe() {
 	echo := sh.Cmd("echo")
-	wc := sh.Cmd("wc", "-w")
-	grep := sh.Cmd("grep", "-o")
 
-	fmt.Print(sh.Pipe(echo("Hi there!!"), grep("Hi"), wc()))
+	// note, you can "bake in" arguments when you create these functions.
+	upper := sh.Cmd("tr", "[:lower:]", "[:upper:]")
+	grep := sh.Cmd("grep")
+
+	// Equivalent of shell command:
+	// $ echo Hi there! | grep -o Hi | wc -w
+	fmt.Print(sh.Pipe(echo(SWCrawl), grep("far"), upper()))
 	// output:
-	// 1
+	// A LONG TIME AGO, IN A GALAXY FAR, FAR AWAY....
 }
 
-func ExampleCmd_BakedIn() {
-	echo := sh.Cmd("echo")
+func ExamplePipeWith() {
+	upper := sh.Cmd("tr", "[:lower:]", "[:upper:]")
+	grep := sh.Cmd("grep")
 
-	// You can bake-in arguments when you create the command, the arguments are
-	// then always used when we use the Command.
-	wc := sh.Cmd("wc", "-w")
-
-	fmt.Print(sh.Pipe(echo("Hi there!"), wc()))
+	fmt.Print(sh.PipeWith(SWCrawl, grep("far"), upper()))
 	// output:
-	// 2
+	// A LONG TIME AGO, IN A GALAXY FAR, FAR AWAY....
 }
 
 func Example_String() {
@@ -50,4 +73,49 @@ func Example_String() {
 	prnt(output.String())
 	// output:
 	// Hi there!
+}
+
+func ExampleDump() {
+	grep := sh.Cmd("grep")
+
+	name := "ExampleDumpTest"
+	defer writeTempFile(name, SWCrawl)()
+
+	// Equivalent of shell command
+	// $ cat ExampleDumpTest | grep far
+	fmt.Print(sh.Pipe(sh.Dump(name), grep("far")))
+	// output:
+	// A long time ago, in a galaxy far, far away....
+}
+
+func ExampleRead() {
+	grep := sh.Cmd("grep")
+
+	name := "ExampleReadTest"
+	f, cleanup := openTempFile(name, SWCrawl)
+	defer cleanup()
+
+	fmt.Print(sh.Pipe(sh.Read(f), grep("far")))
+	// output:
+	// A long time ago, in a galaxy far, far away....
+}
+
+func openTempFile(name, content string) (f *os.File, cleanup func()) {
+	err := ioutil.WriteFile(name, []byte(content), 0777)
+	if err != nil {
+		panic(err)
+	}
+	f, err = os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	return f, func() { f.Close(); os.Remove(name) }
+}
+
+func writeTempFile(name, content string) (cleanup func()) {
+	err := ioutil.WriteFile(name, []byte(content), 0777)
+	if err != nil {
+		panic(err)
+	}
+	return func() { os.Remove(name) }
 }
